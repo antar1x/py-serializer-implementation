fc163c172b493a0509c0f96d0e0736e0a891918c
David <meeddry@gmail.com>
2026-06-05 17:52:56 +0300

    flake8

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from rest_framework.renderers import JSONRenderer
 import io
 from rest_framework.parsers import JSONParser
 
+
 def serialize_car_object(car: Car) -> bytes:
     serializer = CarSerializer(car)
     json = JSONRenderer().render(serializer.data)
